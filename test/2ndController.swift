c5efd8d0102bc1c346e58f69c3ee0d4ba39f8bc1
Joe Gorka <joe.gorka13@gmail.com>
2018-05-19 14:32:29 -0500

    first commit!!

---
+++ test/2ndController.swift
@@ -0,0 +1,15 @@
+//
+//  2ndController.swift
+//  BirthdayApp
+//
+//  Created by Joe Gorka on 5/19/18.
+//  Copyright © 2018 Joe Gorka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondController : UIViewController {
+	
+	
+}
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //properties:
+    
+    @IBOutlet weak var isbirthday: UILabel!
+	
+    @IBOutlet weak var top: UILabel!
+    
+    @IBOutlet weak var date: UIDatePicker!
+    
+    var today : Date = Date()
+    
+    @IBAction func done(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM,dd"
+        let enteredDate : String = formatter.string(from: date.date)
+        let todaystring : String = formatter.string(from: today)
+        if todaystring == enteredDate {
+            isbirthday.text = "Yes!"
+        }
+        else{
+            isbirthday.text = "No :("
+        }
+        isbirthday.sizeToFit()
+    }
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		
+		//add code to segue to other storyboard if user has entered date already
+        isbirthday.text = "Choose a date!"
+        isbirthday.sizeToFit()
     }
 
     override func didReceiveMemoryWarning() {
